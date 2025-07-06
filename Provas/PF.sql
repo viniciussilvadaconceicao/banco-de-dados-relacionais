3c8df1f7f39a916c7270c588bea2377ba9edb133
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2025-07-06 18:39:57 -0300

    Update PF.sql

--- Provas/PF.sql
+++ Provas/PF.sql
@@ -1,35 +1,108 @@
---questao 1 
+--questão 1 criando tabelas 
+
 CREATE TABLE clientes(
     id_cliente SERIAL PRIMARY KEY,
     nome_cliente VARCHAR(100) NOT NULL,
-    telefone VARCHAR(15) NOT NULL,
+    telefone VARCHAR(15) NOT NULL
 );
+
 
 CREATE TABLE jogos(
     id_jogo SERIAL PRIMARY KEY,
-    id_cliente INT NOT NULL REFERENCES clientes(clientes.id_cliente),
+    id_cliente INT NOT NULL REFERENCES clientes(id_cliente),
     nome_jogo VARCHAR(100) NOT NULL,
-    categoria VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
-    quantidade_estoque INT NOT NULL
+    estoque INT NOT NULL
+
 );
 
 CREATE TABLE pedidos(
     id_pedido SERIAL PRIMARY KEY,
-    id_cliente INT NOT NULL REFERENCES clientes(clientes.id_cliente),
-    id_jogo INT NOT NULL REFERENCES jogos(jogos.id_jogo),
-    data_pedido DATE NOT NULL,
-    forma_pagamento VARCHAR(50) NOT NULL,
-    quantidade INT NOT NULL
+    id_cliente INT NOT NULL REFERENCES clientes(id_cliente),
+    id_jogo INT NOT NULL REFERENCES jogos(id_jogo),
+    quantidade_pedido INT NOT NULL,
+    data_pedido DATE NOT NULL ,
+    pagamento VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE log_pedidos(
-    id_pedido_log  SERIAL PRIMARY KEY,
-    id_pedido INT NOT NULL REFERENCES pedidos(pedidos.id_pedido),
-    id_cliente INT NOT NULL REFERENCES clientes(clientes.id_cliente),
-    id_jogo INT NOT NULL REFERENCES jogos(jogos.id_jogo),
-    data_pedido DATE NOT NULL,
-    tipo_operacao VARCHAR(50) NOT NULL
+    id_log SERIAL PRIMARY KEY,
+    id_pedido INT NOT NULL REFERENCES pedidos(id_pedido),
+    id_cliente INT NOT NULL REFERENCES clientes(id_cliente),
+    id_jogo INT NOT NULL REFERENCES jogos(id_jogo),
+    data_pedido DATE NOT NULL ,
+    quantidade_pedido INT NOT NULL,
+    operacao VARCHAR(20) NOT NULL
+
 
 );
 
+-- questão 2 criando a trigger]
+
+CREATE OR REPLACE FUNCTION preencher_log_tabela()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO log_pedidos(id_pedido,id_cliente,id_jogo,data_pedido,quantidade_pedido, operacao)
+    VALUES(NEW.id_pedido, NEW.id_cliente, NEW.id_jogo, NEW.data_pedido, NEW.quantidade_pedido,'INSERT');
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER trigger_preencher_log_tabela
+AFTER INSERT ON pedidos
+FOR EACH ROW
+EXECUTE FUNCTION preencher_log_tabela();
+
+-- questão 3 realizar os insert into
+
+INSERT INTO clientes(nome_cliente, telefone)VALUES
+('lucas pereira', 1198761234),
+('mariana oliveira', 2112345678),
+('joao silva', 3111112222),
+('vinicius silva da conceicao', 2299992222);
+
+INSERT INTO jogos(id_cliente,nome_jogo,categoria,preco,estoque)VALUES
+(1,'THE WITCHER 3','RPG',200, 10),
+(2,'FIFA 24','esporte', 250, 15),
+(1,'cyberpunk 2077','RPG',220, 8),
+(3,'the last of us','aventura', 300, 15),
+(4, 'fortnite', 'tiro', 100, 10);
+
+INSERT INTO pedidos(id_cliente,id_jogo,quantidade_pedido,data_pedido,pagamento)VALUES
+(1,1,1,'2024-09-10','cartão de credito'),
+(2,2,1,'2024-09-12','pix'),
+(1,3,1,'2024-09-10','cartao de credito'),
+(3,4,1,'2024-09-15','boleto'),
+(4,5,1,'2025-06-30','dinheiro');
+
+--questão 4 views
+--primeira view
+CREATE VIEW view_pedidos AS
+SELECT 
+    clientes.id_cliente,
+    clientes.nome_cliente,
+    jogos.nome_jogo,
+    log_pedidos.data_pedido
+
+FROM 
+log_pedidos
+
+JOIN clientes ON log_pedidos.id_cliente = clientes.id_cliente
+JOIN jogos ON log_pedidos.id_jogo = jogos.id_jogo;
+
+-- segunda view
+CREATE VIEW quantidade_total_jogo AS
+SELECT
+    clientes.id_cliente,
+    clientes.nome_cliente,
+    SUM(log_pedidos.quantidade_pedido)
+FROM
+    log_pedidos
+JOIN clientes ON log_pedidos.id_cliente = clientes.id_cliente
+
+GROUP BY
+    clientes.id_cliente,
+    clientes.nome_cliente
+
+ORDER BY
+    clientes.nome_cliente;
